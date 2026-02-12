e4f995824531e339a58e74720000be020a5a8f79
Random-Baka <waynethomson5@gmail.com>
2026-02-12 11:24:18 +0000

    Vehcile reg lookup
    Vehicle reg lookup backend. Not yet saved to DB but data being pulled successfully.

--- src/controllers/vehicleController.js
+++ src/controllers/vehicleController.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file.
+dotenv.config();
 
 /**
  * Common error handler for controller responses.
@@ -40,9 +44,9 @@ export const createVehicleREG = async (req, res) => {
   try {
 
     // DVLA API integration using the registration number provided in the request body
-    const { registrationNumber: _id } = req.params;
+    const { id: registrationNumber } = req.params;
     const liveApi = process.env.APIkey;
-    const DVLAURIlive = 'https://api.dvla.gov.uk/v1/vehicles';
+    const DVLAURIlive = 'https://history.mot.api.gov.uk/v1/trade/vehicles/registration';
     let accessToken = '';
 
     // Access token from Microsoft Identity Platform using the client credentials flow parameters from .env file
@@ -51,27 +55,46 @@ export const createVehicleREG = async (req, res) => {
     const clientSecret = process.env.ClientSecret;
     const scopeURL = process.env.ScopeURL;
 
-    const headers = {
+    let headers = {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
     };
     
-    const payload = JSON.stringify({ 
+    let payload = { 
       grant_type: 'client_credentials',
       client_id: clientId,
       client_secret: clientSecret,
       scope: scopeURL    
-    });
-    
-    const tokenURL = microstoftTokenURL;
-    
-    const { data } = await axios.post(tokenURL, payload, headers);
-    
-    console.log('token API response:', data);
+    };
+  
+    console.log('Requesting access token from Microsoft Identity Platform...');
+    const { data: accessTokenData } = await axios.post(microstoftTokenURL, payload, headers);    
+    console.log('token API response:', accessTokenData);
+
+    // Extract access token from response and check if it exists
+    accessToken = accessTokenData.access_token;
+    if (!accessToken) {throw new Error('Failed to obtain access token'); }
+
+    // Make request to DVLA API with the access token in the Authorization header
+    console.log(`Requesting vehicle data from DVLA API for registration number: ${registrationNumber}...`);
+    headers = {
+      headers: {
+        accept: 'application/json',
+        'x-api-key': liveApi,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    console.log(registrationNumber)
+
+    const { data } = await axios.get(`${DVLAURIlive}/${registrationNumber}`, headers);
+
+    console.log('DVLA API response:', data);
 
     res.status(200).json({ message: 'Vehicle created successfully', data: "This is where the DVLA data will go" });
   } catch (error) {
+    // console.log("Error creating vehicle:", error);
     handleError(res, error, 'Error creating vehicle');
   }
 }
